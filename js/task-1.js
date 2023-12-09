33764dff83ac7d6e2794d6141af5c1017153bbb2
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2023-12-09 13:40:31 +0200

    dz2
    task1 - done, checking - ok
    task2 - done, checking - ok
    task3 - done, checking - ok
    task4 - done, checking - ok

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,22 @@
 "use strict"
+
+let quantity;
+let pricePerDroid;
+let customerCredits;
+let sumAllDriod;
+
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    sumAllDriod = quantity * pricePerDroid;
+    if (sumAllDriod <= customerCredits) {
+        return `You ordered ${quantity} droids worth ${sumAllDriod} credits!`;
+    } else {
+        return "Insufficient funds!";
+    }
+
+}
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
